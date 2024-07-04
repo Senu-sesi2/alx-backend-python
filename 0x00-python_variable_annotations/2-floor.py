8803253196c2d7656cddb35259854fb6ae52fc5e
Senu-sesi2 <senu.sesi2@gmail.com>
2024-07-04 11:51:13 +0000

    return the number floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function floor which takes 
+a float n as argument and returns the 
+floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    return number of floor
+    """
+    return int(n)
